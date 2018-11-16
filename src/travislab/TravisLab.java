8b76b15484b2eb0ca0bc435dc949f4fdc73eb344
Rfinazzo4 <finazzo.ryan4@gmail.com>
2018-11-16 13:43:11 -0800

    Travis lab updates

--- src/travislab/TravisLab.java
+++ src/travislab/TravisLab.java
@@ -15,6 +15,7 @@ public class TravisLab{
      */
     public static void main(String[] args) {
         
+          
         
         System.out.println("Hello World");
         List<String> strList=new ArrayList<>();
