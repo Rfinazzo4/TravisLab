4d8ade653719825ec520b904fdb9a4326c908ba3
Rfinazzo4 <finazzo.ryan4@gmail.com>
2018-11-16 12:28:10 -0800

    Java fx updates,
    Finished the game
    Needs to comply more with MVC but runs fantastic

---
+++ src/travislab/TravisLab.java
@@ -0,0 +1,16 @@
+package travislab;
+
+/**
+ *
+ * @author Ryanfinazzo
+ */
+public class TravisLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
